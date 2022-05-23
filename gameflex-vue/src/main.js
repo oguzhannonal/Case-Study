f5932b517f494792e43ad59a181fdd6f68ff766d
Oğuzhan ÖNAL <oguzhanonal52@hotmail.com>
2022-05-23 04:25:42 +0300

    Update main.js

--- gameflex-vue/src/main.js
+++ gameflex-vue/src/main.js
@@ -2,8 +2,5 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import axios from 'axios';
 
-let baseUrl = "http://localhost:5000/"
-axios.defaults.baseURL = baseUrl;
 createApp(App).mount("#app");
